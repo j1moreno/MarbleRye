23a942b933927242ba97aa8e8b7b5cfd3d8d3f1d
Juan Moreno <jmoreno.0312@gmail.com>
2018-11-16 02:38:37 -0800

    Implemented ListView + adapter for reading DB entries.

---
+++ app/src/main/java/com/j1/breadloaf/MarbleCursorAdapter.java
@@ -0,0 +1,26 @@
+package com.j1.breadloaf;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MarbleCursorAdapter extends SimpleCursorAdapter{
+
+    public MarbleCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to, int flags) {
+        super(context, layout, c, from, to, flags);
+    }
+
+    public void bindView(View view, Context context, Cursor cursor) {
+        super.bindView(view, context, cursor);
+        // convert date from MS to dd/mm/yyyy format:
+        TextView date = view.findViewById(R.id.textViewDate);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(Long.valueOf(date.getText().toString()));
+        date.setText(new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+    }
+}
--- app/src/main/java/com/j1/breadloaf/ReadDBActivity.java
+++ app/src/main/java/com/j1/breadloaf/ReadDBActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -67,21 +69,19 @@ public class ReadDBActivity extends AppCompatActivity {
                     null                                      // don't sort
             );
         }
-
-        if (cursor.moveToFirst()) {
-            while (!cursor.isAfterLast()) {
-                String name = cursor.getString(0) + " | " + cursor.getString(1) + " | " + cursor.getString(2);
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(cursor.getLong(
-                        cursor.getColumnIndexOrThrow(BreadLoafDBContract.Expenses.COLUMN_DATE)));
-                name += " | " + new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()) + "\n";
-                response += name;
-                cursor.moveToNext();
-            }
-        }
-//        Toast.makeText(this, response, Toast.LENGTH_LONG).show();
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(response);
-
+        String[] fromColums = {
+                BreadLoafDBContract.Expenses.COLUMN_AMOUNT,
+                BreadLoafDBContract.Expenses.COLUMN_DESCRIPTION,
+                BreadLoafDBContract.Expenses.COLUMN_DATE
+        };
+        int[] toViews = {
+                R.id.textViewAmt,
+                R.id.textViewDesc,
+                R.id.textViewDate
+        };
+        SimpleCursorAdapter adapter = new MarbleCursorAdapter(this, R.layout.database_read_item,
+                cursor, fromColums, toViews, 0);
+        ListView listView = findViewById(R.id.listViewDB);
+        listView.setAdapter(adapter);
     }
 }
