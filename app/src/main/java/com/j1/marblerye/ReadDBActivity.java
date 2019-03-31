6fdff5203e6cd01bc60641d2ca0cabaca15d916d
Juan Moreno <jmoreno.0312@gmail.com>
2019-03-30 20:16:18 -0700

    Removed redundancies from ReadDBActivity code; implemented wildcard description search.

--- app/src/main/java/com/j1/marblerye/ReadDBActivity.java
+++ app/src/main/java/com/j1/marblerye/ReadDBActivity.java
@@ -72,31 +72,12 @@ public class ReadDBActivity extends AppCompatActivity {
                     "select * from " + MarbleDBContract.Expenses.TABLE_NAME +
                             " order by " + MarbleDBContract.Expenses.COLUMN_DATE + " desc",
                     null);
-            // feed database response into Cursor adapter
-            String[] fromColums = {
-                    MarbleDBContract.Expenses.COLUMN_AMOUNT,
-                    MarbleDBContract.Expenses.COLUMN_DESCRIPTION,
-                    MarbleDBContract.Expenses.COLUMN_DATE
-            };
-            int[] toViews = {
-                    R.id.textViewAmt,
-                    R.id.textViewDesc,
-                    R.id.textViewDate
-            };
-            SimpleCursorAdapter adapter = new MarbleCursorAdapter(this,
-                    R.layout.database_read_item, cursor, fromColums, toViews, 0);
-            ListView listView = findViewById(R.id.listViewDB);
-            listView.setAdapter(adapter);
         } else {
-            // check to see if valid date was entered
-            try {
-                date = MarbleUtils.convertDateToLong(this, searchBox.getText().toString());
-
-                // exception wasn't thrown, start building db query
+            date = MarbleUtils.convertDateToLong(this, searchBox.getText().toString());
+            // check if date is valid:
+            if (date > 0) {
                 String selection =  MarbleDBContract.Expenses.COLUMN_DATE + " == ?";
-
                 String[] selectionArgs = {date + ""};
-
                 cursor = database.query(
                         MarbleDBContract.Expenses.TABLE_NAME,     // The table to query
                         null,                               // The columns to return
@@ -106,53 +87,32 @@ public class ReadDBActivity extends AppCompatActivity {
                         null,                                     // don't filter by row groups
                         null                                      // don't sort
                 );
-                // feed database response into Cursor adapter
-                String[] fromColums = {
-                        MarbleDBContract.Expenses.COLUMN_AMOUNT,
-                        MarbleDBContract.Expenses.COLUMN_DESCRIPTION,
-                        MarbleDBContract.Expenses.COLUMN_DATE
-                };
-                int[] toViews = {
-                        R.id.textViewAmt,
-                        R.id.textViewDesc,
-                        R.id.textViewDate
-                };
-                SimpleCursorAdapter adapter = new MarbleCursorAdapter(this,
-                        R.layout.database_read_item, cursor, fromColums, toViews, 0);
-                ListView listView = findViewById(R.id.listViewDB);
-                listView.setAdapter(adapter);
-            }
-            catch (Exception e) {
-//                Toast.makeText(this, "invalid date input!", Toast.LENGTH_SHORT).show();
-                // was not a date, search descriptions instead:
-                String selection =  MarbleDBContract.Expenses.COLUMN_DESCRIPTION + " == ?";
-                String[] selectionArgs = {searchBox.getText().toString() + ""};
-
+            } else {
                 cursor = database.query(
-                        MarbleDBContract.Expenses.TABLE_NAME,     // The table to query
-                        null,                               // The columns to return
-                        selection,                                // The columns for the WHERE clause
-                        selectionArgs,                            // The values for the WHERE clause
-                        null,                                     // don't group the rows
-                        null,                                     // don't filter by row groups
-                        MarbleDBContract.Expenses.COLUMN_DATE + " desc" // order from latest to oldest
-                );
-                // feed database response into Cursor adapter
-                String[] fromColums = {
-                        MarbleDBContract.Expenses.COLUMN_AMOUNT,
-                        MarbleDBContract.Expenses.COLUMN_DESCRIPTION,
-                        MarbleDBContract.Expenses.COLUMN_DATE
-                };
-                int[] toViews = {
-                        R.id.textViewAmt,
-                        R.id.textViewDesc,
-                        R.id.textViewDate
-                };
-                SimpleCursorAdapter adapter = new MarbleCursorAdapter(this,
-                        R.layout.database_read_item, cursor, fromColums, toViews, 0);
-                ListView listView = findViewById(R.id.listViewDB);
-                listView.setAdapter(adapter);
+                        MarbleDBContract.Expenses.TABLE_NAME,
+                        null,
+                        MarbleDBContract.Expenses.COLUMN_DESCRIPTION + " LIKE ?",
+                        new String[] {"%"+ searchBox.getText().toString() + "%" },
+                        null,
+                        null,
+                        MarbleDBContract.Expenses.COLUMN_DATE + " desc",
+                        null);
             }
         }
+        // feed database response into Cursor adapter
+        String[] fromColums = {
+                MarbleDBContract.Expenses.COLUMN_AMOUNT,
+                MarbleDBContract.Expenses.COLUMN_DESCRIPTION,
+                MarbleDBContract.Expenses.COLUMN_DATE
+        };
+        int[] toViews = {
+                R.id.textViewAmt,
+                R.id.textViewDesc,
+                R.id.textViewDate
+        };
+        SimpleCursorAdapter adapter = new MarbleCursorAdapter(this,
+                R.layout.database_read_item, cursor, fromColums, toViews, 0);
+        ListView listView = findViewById(R.id.listViewDB);
+        listView.setAdapter(adapter);
     }
 }
